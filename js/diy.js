bb5028fcf785f521dcc17c14c7950f9bab7ace34
wzbus <wenzhoubus@qq.com>
2019-07-01 21:45:45 +0800

    iconfont

--- js/diy.js
+++ js/diy.js
@@ -1,8 +1,43 @@
+let mapCenter = [120.70, 28.00];
+let url = location.search;
+if (url) {
+  let x = url.indexOf('lng=');
+  let y = url.indexOf('lat=');
+  if (x && y) {
+    let lng = url.substring(x + 4, y - 1);
+    let lat = url.substring(y + 4);
+    mapCenter = [lng, lat];
+  }
+}
+let map = new maptalks.Map('map', {
+  center: mapCenter,
+  zoom: 14,
+  minZoom: 1,
+  maxZoom: 19,
+  doubleClickZoom: false,
+  spatialReference: {
+    projection: 'baidu'
+  },
+  baseLayer: new maptalks.TileLayer('base', {
+    'urlTemplate': 'http://online{s}.map.bdimg.com/onlinelabel/?qt=tile&x={x}&y={y}&z={z}&styles=pl&scaler=1&p=1',
+    'subdomains': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    'attribution': '&copy; <a target="_blank" href="http://map.baidu.com">Baidu</a>'
+  })
+}).on('mousemove', function (param) {
+  let xy = document.getElementById('xy');
+  xy.innerHTML = param.coordinate.x.toFixed(5) + ',' + param.coordinate.y.toFixed(5);
+});
+let metric = new maptalks.control.Scale({
+  'position': 'bottom-left',
+  'maxWidth': 100,
+  'metric': true,
+  'imperial': false
+});
+map.addControl(metric);
 new Vue({
   el: '#app',
   data () {
     return {
-      map: '',
       origin: 'baidu',
       style: 'none',
       layer: '',
@@ -11,6 +46,7 @@ new Vue({
       mode: '',
       cur: '',
       smooth: 'false',
+      shape: 'circle',
       showSet: '',
       showLayers: false,
       plDefSymbol: {
@@ -43,39 +79,16 @@ new Vue({
         'textName': '文字',
         'textSize': 16,
       },
-      plSymbol: {
-        'lineColor': '#5298FF',
-        'lineWidth': 3,
-        'lineJoin': 'round',
-        'lineCap': 'round',
-        'lineOpacity': 0.8
-      },
-      pgSymbol: {
-        'polygonFill': '#5298FF',
-        'polygonOpacity': 0.5,
-        'lineColor': '#5298FF',
-        'lineWidth': 1,
-        'lineJoin': 'round',
-        'lineCap': 'round',
-        'lineOpacity': 1
-      },
-      poSymbol: {
-        'markerFile': 'pic/station_icon.png',
-        'markerWidth': 11,
-        'markerHeight': 11,
-        'markerOpacity': 1
-      },
-      ptSymbol: {
-        'textFill': '#5298FF',
-        'textName': '文字',
-        'textSize': 16,
-      }
+      plSymbol: {},
+      pgSymbol: {},
+      poSymbol: {},
+      ptSymbol: {}
     }
   },
   methods: {
     setMap () {
       if (this.origin === 'baidu') {
-        this.map.setSpatialReference({
+        map.setSpatialReference({
           projection: 'baidu'
         }).setBaseLayer(new maptalks.TileLayer('base', {
           'urlTemplate': 'http://online{s}.map.bdimg.com/onlinelabel/?qt=tile&x={x}&y={y}&z={z}&styles=pl&scaler=1&p=1',
@@ -84,7 +97,7 @@ new Vue({
           'cssFilter': this.style
         }));
       } else if (this.origin === 'tianditu') {
-        this.map.setSpatialReference({
+        map.setSpatialReference({
           projection: 'EPSG:4326'
         }).setBaseLayer(new maptalks.TileLayer('base', {
           'tileSystem': [1, -1, -180, 90],
@@ -99,34 +112,10 @@ new Vue({
         }));
       }
     },
-    drawpl () {
-      this.mode = 'pl';
-      this.drawTool.setMode('LineString').setSymbol(this.plDefSymbol).enable();
-      this.map.setCursor('crosshair');
-      if (this.cur) {
-        this.cur.endEdit();
-      }
-    },
-    drawpg () {
-      this.mode = 'pg';
-      this.drawTool.setMode('Polygon').setSymbol(this.pgDefSymbol).enable();
-      this.map.setCursor('crosshair');
-      if (this.cur) {
-        this.cur.endEdit();
-      }
-    },
-    drawpo () {
-      this.mode = 'po';
-      this.drawTool.setMode('Point').setSymbol(this.poDefSymbol).enable();
-      this.map.setCursor('crosshair');
-      if (this.cur) {
-        this.cur.endEdit();
-      }
-    },
-    drawpt () {
-      this.mode = 'pt';
-      this.drawTool.setMode('Point').setSymbol(this.ptDefSymbol).enable();
-      this.map.setCursor('crosshair');
+    draw (m, n, s) {
+      this.mode = n;
+      this.drawTool.setMode(m).setSymbol(s).enable();
+      map.setCursor('crosshair');
       if (this.cur) {
         this.cur.endEdit();
       }
@@ -136,6 +125,26 @@ new Vue({
         this.cur.endEdit().setOptions({ 'smoothness': 0.5 });
       } else {
         this.cur.endEdit().setOptions({ 'smoothness': 0 });
+      }
+    },
+    setShape () {
+      if (this.shape === 'circle') {
+        this.poSymbol = {
+          'markerType': 'ellipse',
+          'markerFill': "#ffffff",
+          'markerLineColor': '#5298FF',
+          'markerLineWidth': 2,
+          'markerWidth': 8,
+          'markerHeight': 8,
+          'markerOpacity': 1
+        }
+      } else {
+        this.poSymbol = {
+          'markerFile': 'pic/bubble_icon.png',
+          'markerWidth': 20,
+          'markerHeight': 35,
+          'markerOpacity': 1
+        }
       }
     },
     undo () {
@@ -157,7 +166,7 @@ new Vue({
         let temp = this.layers[i - 1];
         this.layers[i - 1] = this.layers[i];
         this.layers[i] = temp;
-        this.map.sortLayers([...this.layers].reverse());
+        map.sortLayers([...this.layers].reverse());
         this.showLayers = true;
       }
     },
@@ -167,20 +176,20 @@ new Vue({
         let temp = this.layers[i + 1];
         this.layers[i + 1] = this.layers[i];
         this.layers[i] = temp;
-        this.map.sortLayers([...this.layers].reverse());
+        map.sortLayers([...this.layers].reverse());
         this.showLayers = true;
       }
     },
     showLayer (n) {
-      if (this.map.getLayer(n).isVisible()) {
-        this.map.getLayer(n).hide();
+      if (map.getLayer(n).isVisible()) {
+        map.getLayer(n).hide();
       } else {
-        this.map.getLayer(n).show();
+        map.getLayer(n).show();
       }
     },
     removeLayer (n, i) {
       if (confirm("此操作会删除图层，是否继续？")) {
-        this.map.removeLayer(n);
+        map.removeLayer(n);
         this.layers.splice(i, 1);
       } else {
         return false;
@@ -228,53 +237,17 @@ new Vue({
   },
   mounted () {
     let that = this;
-    let mapCenter = [120.70, 28.00];
-    let url = location.search;
-    if (url) {
-      let x = url.indexOf('lng=');
-      let y = url.indexOf('lat=');
-      if (x && y) {
-        let lng = url.substring(x + 4, y - 1);
-        let lat = url.substring(y + 4);
-        mapCenter = [lng, lat];
-      }
-    }
-    this.map = new maptalks.Map('map', {
-      center: mapCenter,
-      zoom: 14,
-      minZoom: 1,
-      maxZoom: 19,
-      doubleClickZoom: false,
-      spatialReference: {
-        projection: 'baidu'
-      },
-      baseLayer: new maptalks.TileLayer('base', {
-        'urlTemplate': 'http://online{s}.map.bdimg.com/onlinelabel/?qt=tile&x={x}&y={y}&z={z}&styles=pl&scaler=1&p=1',
-        'subdomains': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-        'attribution': '&copy; <a target="_blank" href="http://map.baidu.com">Baidu</a>'
-      })
-    }).on('mousemove', function (param) {
-      let xy = document.getElementById('xy');
-      xy.innerHTML = param.coordinate.x.toFixed(5) + ',' + param.coordinate.y.toFixed(5);
-    });
-    let metric = new maptalks.control.Scale({
-      'position': 'bottom-left',
-      'maxWidth': 100,
-      'metric': true,
-      'imperial': false
-    });
-    this.map.addControl(metric);
     this.drawTool = new maptalks.DrawTool({
       mode: 'LineString',
       once: true,
       autoPanAtEdge: true
-    }).addTo(this.map).disable();
+    }).addTo(map).disable();
     let i = 0;
     this.drawTool.on('drawend', function (param) {
       i++;
       that.layers.unshift('图层' + i);
-      new maptalks.VectorLayer('图层' + i).addTo(that.map).bringToFront().addGeometry(param.geometry);
-      that.map.resetCursor();
+      new maptalks.VectorLayer('图层' + i).addTo(map).bringToFront().addGeometry(param.geometry);
+      map.resetCursor();
       that.cur = param.geometry;
       param.geometry.setProperties({ 'mode': that.mode });
       param.geometry.on('dblclick', function () {
